ebebda1481a5917a8208006670d3597d9de4b80d
Alfonso Buononato <74600557+AlfonsoBuononato@users.noreply.github.com>
2021-05-04 18:57:41 +0200

    header e main

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <header>
+        <!-- HEADER LEFT LOGO -->
+        <div class="left">
+            <img src="../assets/dc-logo.png" alt="">
+        </div>
+        <!-- HEADER RIGHT LOGO -->
+        <div class="right">
+            <ul class="list-item">
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped>
+
+    header{
+        display: flex;
+        align-items: center;
+    }
+    /* HEADER LEFT */
+    .left{
+        width: 50%;
+        padding: 10px 20px;
+    }
+
+    .left img{
+        width: 80px;
+    }
+
+    /* HEADER RIGHT */
+
+    .right{
+        width: 50%;
+    }
+
+    .list-item{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .list-item li{
+        display: flex;
+    }
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,50 @@
+<template>
+    <main class="content">
+        <!-- CONTENT TOP -->
+        <div class="content-top">
+            <span>Content goes here</span>
+        </div>
+        <!-- CONTENT BOTTOM -->
+        <div class="content-bottom">
+            <ul>
+                <li>
+                    <a href="#"><img src="../assets/buy-comics-digital-comics.png" alt=""></a>
+                    <span>Digital Comics</span>
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name:"Main",
+}
+</script>
+
+<style scoped>
+    .content{
+        color: white;
+    }
+    /* CONTENT TOP */
+    .content .content-top{
+        height: 100px;
+        font-size: 1.2rem;
+        line-height: 100px;
+        background-color: #000;
+    }
+    /* CONTENT BOTTOM */
+    .content .content-bottom{
+        display: flex;
+        align-items: center;
+        height: 100px;
+        background-color: #0f82f7;
+    }
+    .content .content-bottom li{
+        display: flex;
+        align-items: center;
+    }
+    .content .content-bottom img{
+        width: 50px;
+    }
+</style>
